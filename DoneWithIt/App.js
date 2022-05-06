5075b375a0a18f9fea32aed9982a7fa892b891dc
arielwierz <81646170+arielwierz@users.noreply.github.com>
2022-05-06 21:07:10 +0200

    boton(crear evento)

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>shae</Text>
       <StatusBar style="auto" />
+      <Button>crear evento</Button>
     </View>
   );
 }
