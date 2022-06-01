6a01684adf5b6be988ecaea36f47759e8a66951c
yaetesler <yaetesler@gmail.com>
2022-06-01 19:35:08 -0300

    con log in

--- crypto_front/src/App.js
+++ crypto_front/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button } from 'reactstrap';
+import { Button, Input, Form, FormGroup, Label,s } from 'reactstrap';
 
 
 function App() {
@@ -12,7 +12,52 @@ function App() {
       </header>
       <body className="App-body">
         <div className='container'>
-         <Button color="danger">Danger!</Button>
+          <Form inline>
+            <FormGroup className="mb-2 me-sm-2 mb-sm-0">
+              <Label
+                className="me-sm-2"
+                for="name">
+                name
+              </Label>
+              <Input
+                id="name"
+                name="name"
+                placeholder="your name"
+                type="name"
+              />
+            </FormGroup>
+
+            <FormGroup className="mb-2 me-sm-2 mb-sm-0">
+              <Label
+                className="me-sm-2"
+                for="exampleEmail">
+                Email
+              </Label>
+              <Input
+                id="exampleEmail"
+                name="email"
+                placeholder="crypto@gmail.com"
+                type="email"
+              />
+            </FormGroup>
+            <FormGroup className="mb-2 me-sm-2 mb-sm-0">
+              <Label
+                className="me-sm-2"
+                for="examplePassword"
+              >
+                Password
+              </Label>
+              <Input
+                id="examplePassword"
+                name="password"
+                placeholder="don't tell!"
+                type="password"
+              />
+            </FormGroup><br></br>
+            <Button  color="primary">
+              Submit
+            </Button>
+          </Form>
         </div>
       </body>
   </div>
