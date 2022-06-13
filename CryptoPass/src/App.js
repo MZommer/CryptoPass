3030e490f1f021a0bb79f433551a8bdc20806736
yaetesler <yaetesler@gmail.com>
2022-06-13 08:43:48 -0300

    con bootstrap y form

--- CryptoPass/src/App.js
+++ CryptoPass/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
+
 import './App.css'
 
 import Header from './components/Header'
--- CryptoPass/src/components/CrearEvento.js
+++ CryptoPass/src/components/CrearEvento.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+
 
 export default class CrearEvento extends Component {
     constructor(props) {
@@ -25,7 +30,24 @@ export default class CrearEvento extends Component {
     render() {
     return(
         <div className="page-wrapper">
-        <h1>form</h1>
+        <Container>
+        <h1 className='text-center'>Event </h1>
+            <Row className='text-center'>
+                <Col>
+                <Form>
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                        <Form.Label>Event Name</Form.Label>
+                        <Form.Control type="email" placeholder="name@example.com" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                        <Form.Label>Description</Form.Label>
+                        <Form.Control as="textarea" rows={3} />
+                    </Form.Group>
+                </Form>
+                </Col>
+                <Col>2 of 2</Col>
+            </Row>
+        </Container>
     </div>
     )
 
