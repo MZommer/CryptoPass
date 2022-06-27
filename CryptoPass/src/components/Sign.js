b346d8a4e129a7dbd087419dad6a7234617bd1e8
Matias Zommer <87200156+MZommer@users.noreply.github.com>
2022-06-27 11:23:56 -0300

    Broken state handling
    TODO: Fix the state

--- CryptoPass/src/components/Sign.js
+++ CryptoPass/src/components/Sign.js
@@ -2,19 +2,28 @@ import React, { useEffect, useState } from 'react'
 import Container from 'react-bootstrap/Container';
 import QRCode from "react-qr-code";
 
-
 export default function Sign() {
-    const [qrCode, setQrCode] = useState("");
-    useEffect(() => {
-        const getPass = async () => 
-          setQrCode(await window.eth.generatePass());
-        getPass()
-          .catch(console.error);
-      }, [])
-    return(
+    const generateQR = async setQrCode => {
+        useEffect(() => {
+            const getPass = async () => setQrCode(await window.eth.generatePass());
+            getPass()
+              .catch(console.error);
+          }, [])
+    }
+    const qrLoop = async setQrCode => {
+        const offset = await window.eth.getNextBlockOffset();
+        setTimeout(() => {
+            generateQR(setQrCode)
+        }, offset);
+        qrLoop(setQrCode)
+    }
+    const [qrCode, setQrCode] = useState(null);
+    
+    return (
         <div className="page-wrapper">
+            {generateQR(setQrCode)}
             <Container className="text-center">
-                <QRCode value={qrCode}/>
+                {qrCode ? <QRCode value={qrCode}/> : <div>Sign</div>}       
             </Container>
         </div>
     )
