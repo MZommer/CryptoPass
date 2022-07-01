c39af03f12ba12b9354f54cb77573f693afd1dd2
MZommer <mazokra@gmail.com>
2022-06-30 21:43:42 -0300

    Revert "asd"
    This reverts commit 06b49afaf6855e812b0be3607fa8767f7ff5a770.

--- CryptoPass/src/components/CrearEvento.js
+++ CryptoPass/src/components/CrearEvento.js
@@ -6,6 +6,7 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
 
+
 export default class CrearEvento extends Component {
     constructor(props) {
         super(props)
