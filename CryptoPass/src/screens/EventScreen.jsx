a062597596417dfef93d142152466f261961a290
Matias Zommer <87200156+MZommer@users.noreply.github.com>
2022-11-14 22:27:27 -0300

    Update EventScreen.jsx

--- CryptoPass/src/screens/EventScreen.jsx
+++ CryptoPass/src/screens/EventScreen.jsx
@@ -10,31 +10,40 @@ const DummyEventAddress = "0xEe481D239837B85944912B2dE0685E45644959B1";  // Goer
 export default function EventScreen() {
     const { EtherHelper } = useEtherContext();
     const [eventInfo, setEventInfo] = useState();
-
+    const [addressTokens, setAddressTokens] = useState();
     const [amount, setAmount] = useState(1);
 
-    const buyTickets =      () => {
+    const buyTickets = () => {
         EtherHelper.buyTickets(DummyEventAddress, amount)
             .then(() => toast.success("Ticket minted successfully!!"))
     }
 
     useEffect(() => {
-        if (EtherHelper){
-            console.log("getting event info")
-            EtherHelper.getEventInfo(DummyEventAddress)
-                .then(eventInfo => { setEventInfo(eventInfo); console.log(eventInfo)})
-        }
+        if (!EtherHelper)
+            return;
+
+        console.log("getting event info")
+        EtherHelper.getEventInfo(DummyEventAddress)
+            .then(_eventInfo => setEventInfo(_eventInfo))
+            .then(() => console.log(eventInfo))
+            .catch(console.error)
+        EtherHelper.getAddressTokens(DummyEventAddress)
+            .then(_addressTokens => setAddressTokens(_addressTokens))
+            .then(() => console.log(addressTokens))
+            .catch(console.error)
     }, [EtherHelper])
 
-    if (!eventInfo )
+    if (!eventInfo)
         return <Loading/>
-
     return ( 
         <div>
             <h1>{eventInfo.Title}</h1>
             <h3>{eventInfo.Description}</h3>
 
             <button onClick={buyTickets}>Comprar entradas</button>
+            { !addressTokens? 
+                <div>Owned Tickets {addressTokens.join(", ")}</div> 
+                : null}
         </div>
     )
 }
