b9869b08699327368eb15bd1a7abe48c43dedc1a
Matias Zommer <87200156+MZommer@users.noreply.github.com>
2022-11-14 22:27:19 -0300

    Update EtherHelper.js

--- CryptoPass/src/utils/EtherHelper.js
+++ CryptoPass/src/utils/EtherHelper.js
@@ -18,9 +18,34 @@ export default class EtherHelper {
         return eventInfo;
     }
 
+    async getAddressTokens(eventAddress) {
+        const EVT = new ethers.Contract(eventAddress, EVT_ABI, this.etherProvider);
+        const tokens = EVT.getAddressTokens(await this.getAddress());
+        return tokens;
+
+    }
     async buyTickets(eventAddress, amount) {
         const EVT = new ethers.Contract(eventAddress, EVT_ABI, this.etherProvider.getSigner(), {value: 2000*amount});
         return await EVT.mint(await EVT.getActiveSaleId(), await this.getAddress(), amount); // missing the sending of the money for the ticket
     }
 
+
+
+    async generatePass(tokenId){
+        const signer = this._etherProvider.getSigner();
+        const signature = await signer.signMessage(tokenId);
+        const address = await signer.getAddress();
+        const pass = btoa(`${signature};${address};${tokenId}`);
+        return `CPT ${pass}`;  // Crypto Pass Token
+    }
+
+    async valdatePass(pass, address){
+        if (!pass.startsWith("CPT"))
+            return false;
+        const [signature, address, tokenId] = atob(pass.split(" ")[1]).split(";");
+        const signerAdress = await ethers.utils.verifyMessage(tokenId, signature);
+        const EVT = new ethers.Contract(address, EVT_ABI, this._etherProvider);
+        const owner = await EVT.ownerOf(tokenId);
+        return signerAdress === owner;
+    }
 }
