8c4451e61bf9f46f2a716b70a82a46b13a51f495
yaetesler <yaetesler@gmail.com>
2022-11-16 09:24:50 -0300

    ssidebar

--- CryptoPass/src/App.jsx
+++ CryptoPass/src/App.jsx
@@ -143,31 +143,7 @@ export default function App() {
         <List>
           {['Eventos', 'Read Qr', ].map((text, index) => (
             <ListItem key={text} disablePadding sx={{ display: 'block' }}>
-              <ListItemButton
-                sx={{
-                  minHeight: 48,
-                  justifyContent: open ? 'initial' : 'center',
-                  px: 2.5,
-                }}
-              >
-                <ListItemIcon
-                  sx={{
-                    minWidth: 0,
-                    mr: open ? 3 : 'auto',
-                    justifyContent: 'center',
-                  }}
-                >
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem key={text} disablePadding sx={{ display: 'block' }}>
+             
               <ListItemButton
                 sx={{
                   minHeight: 48,
